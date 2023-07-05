0e37efb576abd5ee8395566d4cb2cf3ef45668d6
enversezgin <enversezgin81@gmail.com>
2023-07-05 13:58:40 +0300

    day34

---
+++ src/main/java/day33exceptionsenumsiterator/InvalidStudentGradeException.java
@@ -0,0 +1,11 @@
+package day33exceptionsenumsiterator;
+
+public class InvalidStudentGradeException extends Exception {
+
+    public InvalidStudentGradeException(String massage){
+        super(massage);
+    }
+
+
+    
+}
